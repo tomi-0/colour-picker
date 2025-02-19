a56ba107a54fe02df0029688395f9fdd097d1b98
tomi-0 <asadetomi@gmail.com>
2025-02-19 01:44:07 +0000

    added css styling

--- colour-picker/src/ColourPicker.jsx
+++ colour-picker/src/ColourPicker.jsx
@@ -4,17 +4,20 @@ function ColourPicker(){
     const[color, setColor] = useState("#FFFFFF");
 
     function changeColour (event) {
-        setColor(event.target.va);
+        setColor(event.target.value);
     }
 
     return (
         <div className="colour-picker-container">
-            <h1>Colour Picker Page</h1>
+            <h1>Colour Picker</h1>
             <div className="colour-box" style={{backgroundColor: color}}>
                 <p>Selected Colour : {color}</p>
             </div>
-            <label>Select Colour :</label>
-            <input type="color" onChange={changeColour} value={color}></input>
+            <div className="box"></div>
+            <div className="colour-select">
+                <label>Select Colour : </label>
+                <input type="color" onChange={changeColour} value={color}></input>
+            </div>
         </div>
     )
 }
